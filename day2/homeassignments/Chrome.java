cab05b9a9bd96f5a964a81c5fa545fa0a741d3b8
subbu23121990 <subhu109@gmail.com>
2024-09-11 08:58:34 +0100

    Add files via upload

---
+++ day2/homeassignments/Chrome.java
@@ -0,0 +1,18 @@
+package week1.day2.homeassignments;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+
+		float versionOfType= 91.0f;
+		String developerOfType="Google";
+		Boolean isBeta=false;
+		int isReleaseYear=2008;
+		char shortcutKey  ='C';
+		
+		
+		System.out.println(versionOfType +" \n "+developerOfType+" \n "+isBeta+" \n "+isReleaseYear+" \n "+shortcutKey);
+
+	}
+
+}
