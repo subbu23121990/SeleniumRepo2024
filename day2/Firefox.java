1c3bf96caa9b529d2755c276913b0600cae5731c
subbu23121990 <subhu109@gmail.com>
2024-09-11 05:34:32 +0100

    Add files via upload

---
+++ day2/Firefox.java
@@ -0,0 +1,18 @@
+package week1.day2;
+
+public class Firefox 
+{
+
+	public static void main(String[] args) 
+	{
+	
+		   float browserVersion = 100.2f  ;
+		   boolean isVisible = true;
+		   int releaseYear = 1998;
+		   char browserLogo = 'f' ;
+		   
+		   System.out.println(browserVersion+" " +isVisible+ " " +releaseYear+" "+browserLogo);
+
+	}
+
+}
